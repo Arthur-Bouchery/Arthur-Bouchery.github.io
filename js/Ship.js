573067875a31b03964ab6d30c77db7ddec0ef79a
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-10 09:50:47 +0200

    Update Ship.js

--- js/Ship.js
+++ js/Ship.js
@@ -1,3 +1,4 @@
+const Entity = require("./Entity");
 class Ship extends Entity{//refactor ship factory
   constructor(Name, hardpoints, weapons, shields, Parent, Height, Width, Mass, PosX, PosY, RotZ) {
     super(Parent, Height, Width, Mass, PosX, PosY, RotZ);
@@ -7,7 +8,7 @@ class Ship extends Entity{//refactor ship factory
     this.children= this.children.concat(weapons);
     this.shields = shields;
   }
-  function render(ctx){
+  render(ctx){
     super(ctx);
   }
 }
