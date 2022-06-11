bd9fadab99639d4d662cbff138bbae5be89b8a52
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-11 19:35:38 +0200

    good now?

--- js/Game.js
+++ js/Game.js
@@ -1,6 +1,6 @@
 class Game{
   constructor(){
-    this.startTime = false;
+    this.startTime = 0;
     this.simulationTime = 0;
     this.lastFrameTime = 0;
     this.started = false;
@@ -8,26 +8,29 @@ class Game{
   }
   static start(){
     const canvas = document.getElementById("gameView");
+    var ctx = canvas.getcontext('2d');
     console.log(canvas.width+" "+canvas.height);
     var grid = new Grid(canvas.width, canvas.height);
     var instance = new Game(Grid);
     var starter = Ship.getStarter();
     instance.addElement(starter);
-    this.startTime = Date.getTime();
+    this.startTime = Date.now();
     console.log(this.startTime);
     this.started = true;
-    setInterval(this.render(),50);
+    setInterval(this.render(ctx),50);
   }
   addElement(e){
     this.elements.push(e);
   }
-  render(){
-    var ctx = this.gameGrid.getcontext('2d');
+  render(ctx){
+    
     this.gameGrid.render(ctx);
   }
   getTime(){
     if(started){
-      return Date.getTime()-this.startTime;
+      let temp = Date.now()-this.startTime;
+      console.log("game Time : "+temp);
+      return temp;
     }else{
       console.log("not started yet");
     }
