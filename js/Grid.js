7d8b5cba945a46b3f40b8b497c9e2d29102f01c7
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-11 14:39:01 +0200

    chasse aux erreurs

--- js/Grid.js
+++ js/Grid.js
@@ -1,8 +1,8 @@
 class Grid{
   constructor(width, height){
-    if (this.constructor === Grid) {
-      throw new TypeError('Abstract class "Grid" cannot be instantiated directly');
-    }
+    //if (this.constructor === Grid) {
+      //throw new TypeError('Abstract class "Grid" cannot be instantiated directly');
+    //}
     this.width=width;
     this.height=height;
   }
