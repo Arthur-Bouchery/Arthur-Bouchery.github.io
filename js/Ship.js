05d0e2620da5cc98a148bb889f2b33fc7e395250
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-11 23:19:06 +0200

    added a ship position tracker to console output

--- js/Ship.js
+++ js/Ship.js
@@ -6,6 +6,7 @@ class Ship extends Entity{//refactor ship factory
     this.weapons = weapons; 
     this.children= this.children.concat(weapons);
     this.shields = shields;
+    setinterval(console.log(this.name+"coordinates : "+this.posX+" "+this.posY), 1000);
   }
   static getStarter(Grid, x, y){
     return new Ship("Starter", null, null, null, Grid, 100, 30, 50000, x, y, 0);
