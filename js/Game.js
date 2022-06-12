44eca4fef52fabe4bfa6b554c2f3dc99d292013b
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-12 14:41:08 +0200

    sadness

--- js/Game.js
+++ js/Game.js
@@ -34,19 +34,6 @@ class Game{
       if(name=="a"){instance.playerShip.move(0,0,1);};
       if(name=="e"){instance.playerShip.move(0,0,-1);};
     }, false);
-    // Add event listener on keyup
-    document.addEventListener('keydown', (event) => {
-      var name = event.key;
-      var code = event.code;
-      // Console.log the key name and key code on keydown
-      console.log(`Key released ${name} \r\n Key code value: ${code}`);
-      if(name=="z"){instance.playerShip.move(0,-1,0);};
-      if(name=="s"){instance.playerShip.move(0,1,0);};
-      if(name=="q"){instance.playerShip.move(1,0,0);};
-      if(name=="d"){instance.playerShip.move(-1,0,0);};
-      if(name=="a"){instance.playerShip.move(0,0,-1);};
-      if(name=="e"){instance.playerShip.move(0,0,1);};
-    }, false);
 
     //démarrage du rendu
     instance.startTime = Date.now();
