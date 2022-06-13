8794715082d25416f70b3e5ad64c9f414d427267
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2022-06-13 17:15:04 +0200

    pj

--- js/Ship.js
+++ js/Ship.js
@@ -18,7 +18,7 @@ class Ship extends Entity{//refactor ship factory
     let a=this.rotZ*Math.PI/180;
     console.log(a);
     let newVX=vX*Math.cos(a)+vY*Math.sin(a);
-    let newVY=vX*(1-Math.sin(a))+(1-vY*Math.cos(a));
+    let newVY=vX*Math.sin(a)+vY*Math.cos(a);
     console.log(newVX+"         "+newVY+"   cos/sin: "+Math.cos(a)+" "+Math.sin(a));
     this.move(newVX,newVY,0);
   }
